70acde2a3badbfd7d10dc629a4586d625e16d427
Warrick Smith <warrick.smith@neo.nz>
2023-03-24 21:22:28 +1300

    chore: update ports

--- server/src/server.js
+++ server/src/server.js
@@ -1,7 +1,7 @@
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5005;
 const dbPort = 27017;
-const app = require("./app");
-const mongoose = require("mongoose");
+const app = require('./app');
+const mongoose = require('mongoose');
 const dbURI = process.env.MONGO_URI || `mongodb://localhost:${dbPort}/mongo`;
 
 const connectDB = async () => {
@@ -10,30 +10,30 @@ const connectDB = async () => {
 
     // When successfully connected - initial connection
     console.log(
-      "\x1b[36m%s\x1b[0m",
+      '\x1b[36m%s\x1b[0m',
       `Mongoose default connection is open to: ${dbURI}`
     );
 
     // When successfully connected - Subsequent connections
-    mongoose.connection.on("connected", () => {
+    mongoose.connection.on('connected', () => {
       console.log(
-        "\x1b[36m%s\x1b[0m",
+        '\x1b[36m%s\x1b[0m',
         `Mongoose default connection is open to: ${dbURI}`
       );
     });
 
     // When the connection is disconnected
-    mongoose.connection.on("disconnected", () => {
+    mongoose.connection.on('disconnected', () => {
       console.log(
-        "\x1b[33m%s\x1b[0m",
+        '\x1b[33m%s\x1b[0m',
         `Mongoose default connection disconnected from: ${dbURI}`
       );
     });
   } catch (err) {
     // Show error if no initial connection to the MongoDb can be established
     console.log(
-      "\x1b[31m%s\x1b[0m",
-      "Error connecting to MongooDB. Please check database is running and available."
+      '\x1b[31m%s\x1b[0m',
+      'Error connecting to MongooDB. Please check database is running and available.'
     );
   }
 };
@@ -42,5 +42,5 @@ const connectDB = async () => {
 connectDB();
 
 app.listen(port, () => {
-  console.log("\x1b[32m", `API server started at http://localhost:${port}`);
+  console.log('\x1b[32m', `API server started at http://localhost:${port}`);
 });
