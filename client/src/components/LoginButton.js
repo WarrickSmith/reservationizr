86dd879ee84c5ac14b37ea892c75d8f56e0c33e1
Warrick Smith <warrick.smith@neo.nz>
2023-10-18 13:23:17 +1300

    fix: add login account for demo

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -1,13 +1,13 @@
 // This component displays a Log In/Log out button in the page header. It's status is dependant on the user being successfully authenticated by auth0
 
-import './LoginButton.css';
-import { useAuth0 } from '@auth0/auth0-react';
+import './LoginButton.css'
+import { useAuth0 } from '@auth0/auth0-react'
 
 const LoginButton = () => {
-  const { loginWithRedirect, logout, isAuthenticated, isLoading } = useAuth0();
+  const { loginWithRedirect, logout, isAuthenticated, isLoading } = useAuth0()
 
   if (isLoading) {
-    return <p>Loading login details...</p>;
+    return <p>Loading login details...</p>
   }
 
   if (isAuthenticated) {
@@ -24,14 +24,17 @@ const LoginButton = () => {
       >
         Log Out
       </button>
-    );
+    )
   }
 
   return (
-    <button className="btn-login" onClick={() => loginWithRedirect()}>
-      Log In
-    </button>
-  );
-};
+    <>
+      <button className="btn-login" onClick={() => loginWithRedirect()}>
+        Log In
+      </button>
+      <p class="text-small">( guest@guest.com pw: guest )</p>
+    </>
+  )
+}
 
-export default LoginButton;
+export default LoginButton
